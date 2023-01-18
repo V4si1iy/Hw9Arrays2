29b4ded521e7aed46fa5121c146259e84b22a5f1
Vasiliy <pilaroiding@gmail.com>
2023-01-18 18:15:54 +0300

    Hw9 done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+        task2();
+        task3();
+        task4();
     }
 
     public static int[] generateRandomArray() {
@@ -12,6 +15,49 @@ public class Main {
         return arr;
     }
 
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum);
+    }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        int spendMax = arr[0];
+        int spendMin = arr[0];
+        for (int i : arr) {
+            if (spendMin > i)
+                spendMin = i;
+            if (spendMax < i)
+                spendMax = i;
+        }
+        System.out.println("Минимальная сумма трат за день составила " + spendMin);
+        System.out.println("Максимальная сумма трат за день составила " + spendMax);
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] arr = generateRandomArray();
+        double spendAverage = arr[0];
+        for (int i : arr) {
+            spendAverage += i;
+        }
+        spendAverage /= 30;
+        System.out.printf("Средняя сумма трат за месяц составила %.2f ", spendAverage);
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+    }
 
 }
